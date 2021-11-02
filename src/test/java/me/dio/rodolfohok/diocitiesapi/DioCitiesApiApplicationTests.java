568d62c7419eef797dcf224b0f6b65a406276c37
rodolfoHOk <rodolfo.okino@gmail.com>
2021-11-02 19:58:46 -0300

    first commit

---
+++ src/test/java/me/dio/rodolfohok/diocitiesapi/DioCitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.rodolfohok.diocitiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioCitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
